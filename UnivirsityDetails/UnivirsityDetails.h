23fd59585d7d6ffea38e82f140b325ec7649fa77
Abanoub Habib <Abanoub.Habib@warbabank.com>
2024-05-03 19:40:13 +0300

    initial

---
+++ UnivirsityDetails/UnivirsityDetails.h
@@ -0,0 +1,18 @@
+//
+//  UnivirsityDetails.h
+//  UnivirsityDetails
+//
+//  Created by Abanoub Nabil on 5/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnivirsityDetails.
+FOUNDATION_EXPORT double UnivirsityDetailsVersionNumber;
+
+//! Project version string for UnivirsityDetails.
+FOUNDATION_EXPORT const unsigned char UnivirsityDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnivirsityDetails/PublicHeader.h>
+
+
