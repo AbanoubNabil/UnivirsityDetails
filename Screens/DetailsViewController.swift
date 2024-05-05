074c5104c0dd632bd548bf1a1ca74e9a4d71c23e
Abanoub Habib <Abanoub.Habib@warbabank.com>
2024-05-05 13:26:41 +0300

    initialize module

---
+++ Screens/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  UnivirsityDetails
+//
+//  Created by Abanoub Nabil on 5/3/24.
+//
+
+import UIKit
+
+public class DetailsViewController: UIViewController {
+
+    // MARK: - Init -
+    convenience init() {
+        self.init(nibName: "\(Self.self)", bundle: Bundle(for: Self.self))
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
